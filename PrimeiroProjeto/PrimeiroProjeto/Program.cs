676f3e89b3433df66bc543af253c67d1f0c5faab
Vinicius Pereira <vinicius.pereira@gotobiz.com.br>
2024-09-06 09:57:39 -0300

    feat: meu primeiro código em csharp

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroProjeto // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 28;
+            string nome = "Vinicius Pereira";
+
+            Console.WriteLine($"Olá, meu nome é {nome}. Tenho {idade} anos");
+        }
+    }
+}
