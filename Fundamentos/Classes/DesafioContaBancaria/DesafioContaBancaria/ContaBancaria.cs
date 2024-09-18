652134d781efad30bd6c964ba8f92d34962e4ce9
Vinicius Pereira <vinicius.pereira@gotobiz.com.br>
2024-09-17 22:03:57 -0300

    final do codigo do encapsulamento - conta bancaria

---
+++ Fundamentos/Classes/DesafioContaBancaria/DesafioContaBancaria/ContaBancaria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace DesafioContaBancaria
+{
+    class ContaBancaria
+    {
+        public int Numero {  get; private set; }
+        public string Titular { get; set; }
+
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo, double depositoInicial) : this(numero, titular) {
+            Deposito(depositoInicial);
+            
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+
+        public override string ToString() {
+            return "Conta "
+            + Numero
+            + ", Titular: "
+            + Titular
+            + ", Saldo: R$"
+            + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        
+        }
+
+    }
+}
--- Fundamentos/Classes/DesafioContaBancaria/DesafioContaBancaria/Program.cs
+++ Fundamentos/Classes/DesafioContaBancaria/DesafioContaBancaria/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesafioContaBancaria;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaBancaria conta;
+
+            Console.WriteLine("Entre com o número da conta:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do titular da conta:");
+            string titular = Console.ReadLine();
+
+            Console.WriteLine("Deseja efetuar um deposito inicial ?");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.WriteLine("Digite o valor desejado para depósito:");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+
+            }
+            else {
+                conta = new ContaBancaria(numero, titular, 0.00);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para depósito:");
+            double quantia = double.Parse(Console.ReadLine());
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+
+        }
+    }
+}
