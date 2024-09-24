b816292bd5cd872f3ef3aa673e92907b18f00787
Vinicius Pereira <vinicius.pereira@gotobiz.com.br>
2024-09-24 15:24:04 -0300

    feat: .

---
+++ enumeracao-composicao/exercicio-proposto/exercicio-proposto/Entities/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_proposto.Entities
+{
+    class Client
+    {
+        public  string Name { get; private set; }
+        public string Email { get; set; }
+
+
+        public Client(string name, string email) {
+            Name = name;
+            Email = email;
+        }
+
+
+    }
+}
---
+++ enumeracao-composicao/exercicio-proposto/exercicio-proposto/Entities/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_proposto.Entities
+{
+    class OrderItem
+    {
+        public int quantity { get; set; }
+        public double price { get; private set; }
+
+
+        public double subTotal() {
+            return quantity * price;
+        
+        }
+    }
+}
