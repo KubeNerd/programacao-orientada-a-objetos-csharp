faa64fafb7c19ae4f7e44d047b1362717174fede
Vinicius Pereira <vinicius.pereira@gotobiz.com.br>
2024-09-29 17:28:59 -0300

    finalizando exercicio proposto sobre herança e polimorfismo

---
+++ heranca/desafio-proposto-heranca-polimorfismo/desafio-proposto-heranca-polimorfismo/Entities/ImportedProduct.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+namespace desafio_proposto_heranca_polimorfismo.Entities
+{
+    class ImportedProduct: Product
+    {
+        public double CustomsFee { get; set; }
+
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customsFree)
+            : base(name, price)
+        {
+
+            CustomsFee = customsFree;
+
+        }
+
+
+        public double TotalPrice() 
+        { 
+            
+            return Price + CustomsFee; 
+        
+        }
+
+
+        public override string PriceTag()
+        {
+            return Name 
+                + " R$ " 
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Custom free): R$ "
+                + CustomsFee.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }   
+
+
+    }
+}
